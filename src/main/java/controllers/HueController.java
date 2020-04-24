6b7ff10fd186dbfa9a8c899576b6d105eb50ec54
Joshua Pugsley <Joshua@ibm.com>
2020-04-24 19:07:46 -0400

    MEMENOBIGBOY

--- src/main/java/controllers/HueController.java
+++ src/main/java/controllers/HueController.java
@@ -13,7 +13,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
-//me me big boy
+
 import static serializers.HueLightSerializer.serialize;
 
 public class HueController {
